65cd956279fd21a2c9249d4c20094c19ffe9263f
KeitonChaves <keiton.chaves.16@gmail.com>
2024-06-11 21:53:47 -0400

    V3

--- crud_empleados.py
+++ crud_empleados.py
@@ -5,10 +5,45 @@ os.system("cls")
 def cargar_json(url_archivo):
     with open(url_archivo, 'r') as archivo:
         return json.load(archivo)
+    
+def menu_general():
+    os.system("cls")
+    print("1. Crear empleado")
+    print("2. Actualizar empleado")
+    print("3. Eliminar empleado")
+    print("4. Listar empleado")
+    print("5. Salir")
 
-def iniciar_programa():    
-    empleados = cargar_json('./CRUD/empleado.json')
+def seleccionar_opcion(max_option):
+    opcion = 0
+    while True:
+        try:
+            opcion = int(input("Seleccione una opción >>> "))
+        except:
+            pass
+        if opcion < 1 or opcion > max_option:
+            input("Opción inválida, intente nuevamente >>> ")
+        else:
+            return opcion
 
-    print(empleados)
+def iniciar_programa():
+    while True:
+        menu_general()
+        opcion = seleccionar_opcion(5)
+        if opcion == 1:
+            print("1. Crear empleado")
+        elif opcion == 2:
+            print("2. Actualizar empleado")
+        elif opcion == 3:
+            print("3. Eliminar empleado")
+        elif opcion == 4:
+            print("4. Listar empleado")
+        elif opcion == 5:
+            return
+        input()
+
+        empleados = cargar_json('./CRUD/empleado.json')
+
+
 
 iniciar_programa()
