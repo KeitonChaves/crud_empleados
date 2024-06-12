0876a8382b5bde759a7e3309354e1c9f9b7ac250
KeitonChaves <keiton.chaves.16@gmail.com>
2024-06-11 21:39:55 -0400

    Version 2

--- crud_empleados.py
+++ crud_empleados.py
@@ -5,7 +5,10 @@ os.system("cls")
 def cargar_json(url_archivo):
     with open(url_archivo, 'r') as archivo:
         return json.load(archivo)
-    
-empleados = cargar_json('./CRUD/empleado.json')
 
-print(empleados)
+def iniciar_programa():    
+    empleados = cargar_json('./CRUD/empleado.json')
+
+    print(empleados)
+
+iniciar_programa()
